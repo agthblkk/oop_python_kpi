d5c0dfe0f45bbc0020770a3029254ae44a9bbe93
agthblkk <101601410+agthblkk@users.noreply.github.com>
2022-11-01 12:34:24 +0200

    Update tas02LAB02_PART01.py

--- tas02LAB02_PART01.py
+++ tas02LAB02_PART01.py
@@ -1,3 +1,6 @@
+import datetime
+
+
 class Pizza:
     def __init__(self, quantity):
         if isinstance(quantity, int):
@@ -18,24 +21,28 @@ class Pizza:
         return '{self.base}, {self.size} cm'.format(self=self)
 
 
-class potd1(Pizza):
+class MondayP(Pizza):
     def __init__(self, quantity):
         super().__init__(quantity)
         self.price = 100
-        self.ingr1 = "tomatoes"
-        self.ingr2 = "cucumbers"
-        self.ingr3 = "chicken"
-        self.sause = "ketchup"
+        self.ingr1 = "tomatoes "
+        self.ingr2 = "cucumbers "
+        self.ingr3 = "chicken "
+        self.sause = "ketchup "
         self.additional = ""
+        self.addPrice = 10
 
     def showIng(self):
-        return self.ingr1 + " " + self.ingr2 + " " + self.ingr3 + " " + self.sause
+        return self.ingr1 + self.ingr2 + self.ingr3 + self.sause
 
     def priceCheck(self):
-        return self.price * self.q
+        if self.additional == "":
+            return str(self.price * self.q)
+        else:
+            return str((self.price + self.addPrice) * self.q)
 
     def addIng(self, ingr):
-        self.additional += ingr
+        self.additional += " " + ingr
 
     def customerPizza(self):
         if self.additional == "":
@@ -43,27 +50,33 @@ class potd1(Pizza):
         else:
             wholeP = self.showIng() + self.additional
         return wholeP
+
     def __str__(self):
         return '{self.base}, {self.size} cm, {self.price}'.format(self=self)
 
-class potd2(Pizza):
+
+class TuesdayP(Pizza):
     def __init__(self, quantity):
         super().__init__(quantity)
         self.price = 90
-        self.ingr1 = "lettuces"
-        self.ingr2 = "pumpkin"
-        self.ingr3 = "chicken slices"
-        self.sause = "bbq"
+        self.ingr1 = "lettuces "
+        self.ingr2 = "pumpkin "
+        self.ingr3 = "chicken slices "
+        self.sause = "bbq "
         self.additional = ""
+        self.addPrice = 10
 
     def showIng(self):
-        return self.ingr1 + " " + self.ingr2 + " " + self.ingr3 + " " + self.sause
+        return self.ingr1 + self.ingr2 + self.ingr3 + self.sause
 
     def priceCheck(self):
-        return self.price * self.q
+        if self.additional == "":
+            return str(self.price * self.q)
+        else:
+            return str((self.price + self.addPrice) * self.q)
 
     def addIng(self, ingr):
-        self.additional += ingr
+        self.additional += " " + ingr
 
     def customerPizza(self):
         if self.additional == "":
@@ -71,27 +84,33 @@ class potd2(Pizza):
         else:
             wholeP = self.showIng() + self.additional
         return wholeP
+
     def __str__(self):
         return '{self.base}, {self.size} cm, {self.price}'.format(self=self)
 
-class potd3(Pizza):
+
+class WednesdayP(Pizza):
     def __init__(self, quantity):
         super().__init__(quantity)
         self.price = 130
-        self.ingr1 = "onion"
-        self.ingr2 = "corn"
-        self.ingr3 = "chicken slices"
-        self.sause = "bbq"
+        self.ingr1 = "onion "
+        self.ingr2 = "corn "
+        self.ingr3 = "chicken slices "
+        self.sause = "bbq "
         self.additional = ""
+        self.addPrice = 10
 
     def showIng(self):
-        return self.ingr1 + " " + self.ingr2 + " " + self.ingr3 + " " + self.sause
+        return self.ingr1 + self.ingr2 + self.ingr3 + self.sause
 
     def priceCheck(self):
-        return self.price * self.q
+        if self.additional == "":
+            return str(self.price * self.q)
+        else:
+            return str((self.price + self.addPrice) * self.q)
 
     def addIng(self, ingr):
-        self.additional += ingr
+        self.additional += " " + ingr
 
     def customerPizza(self):
         if self.additional == "":
@@ -103,24 +122,175 @@ class potd3(Pizza):
     def __str__(self):
         return '{self.base}, {self.size} cm, {self.price}'.format(self=self)
 
+
+class ThursdayP(Pizza):
+    def __init__(self, quantity):
+        super().__init__(quantity)
+        self.price = 130
+        self.ingr1 = "onion "
+        self.ingr2 = "corn "
+        self.ingr3 = "chicken slices "
+        self.sause = "bbq "
+        self.additional = ""
+        self.addPrice = 10
+
+    def showIng(self):
+        return self.ingr1 + self.ingr2 + self.ingr3 + self.sause
+
+    def priceCheck(self):
+        if self.additional == "":
+            return str(self.price * self.q)
+        else:
+            return str((self.price + self.addPrice) * self.q)
+
+    def addIng(self, ingr):
+        self.additional += " " + ingr
+
+    def customerPizza(self):
+        if self.additional == "":
+            wholeP = self.showIng()
+        else:
+            wholeP = self.showIng() + self.additional
+        return wholeP
+
+    def __str__(self):
+        return '{self.base}, {self.size} cm, {self.price}'.format(self=self)
+
+
+class FridayP(Pizza):
+    def __init__(self, quantity):
+        super().__init__(quantity)
+        self.price = 130
+        self.ingr1 = "onion "
+        self.ingr2 = "corn "
+        self.ingr3 = "chicken slice "
+        self.sause = "bbq "
+        self.additional = ""
+        self.addPrice = 10
+
+    def showIng(self):
+        return self.ingr1 + self.ingr2 + self.ingr3 + self.sause
+
+    def priceCheck(self):
+        if self.additional == "":
+            return str(self.price * self.q)
+        else:
+            return str((self.price + self.addPrice) * self.q)
+
+    def addIng(self, ingr):
+        self.additional += " " + ingr
+
+    def customerPizza(self):
+        if self.additional == "":
+            wholeP = self.showIng()
+        else:
+            wholeP = self.showIng() + self.additional
+        return wholeP
+
+    def __str__(self):
+        return '{self.base}, {self.size} cm, {self.price}'.format(self=self)
+
+
+class SaturdayP(Pizza):
+    def __init__(self, quantity):
+        super().__init__(quantity)
+        self.price = 130
+        self.ingr1 = "onion "
+        self.ingr2 = "chicken bones "
+        self.ingr3 = "chicken slices "
+        self.sause = "bbq "
+        self.additional = ""
+        self.addPrice = 10
+
+    def showIng(self):
+        return self.ingr1 + self.ingr2 + self.ingr3 + self.sause
+
+    def priceCheck(self):
+        if self.additional == "":
+            return str(self.price * self.q)
+        else:
+            return str((self.price + self.addPrice) * self.q)
+
+    def addIng(self, ingr):
+        self.additional += " " + ingr
+
+    def customerPizza(self):
+        if self.additional == "":
+            wholeP = self.showIng()
+        else:
+            wholeP = self.showIng() + self.additional
+        return wholeP
+
+    def __str__(self):
+        return '{self.base}, {self.size} cm, {self.price}'.format(self=self)
+
+
+class SundayP(Pizza):
+    def __init__(self, quantity):
+        super().__init__(quantity)
+        self.price = 130
+        self.ingr1 = "garlic "
+        self.ingr2 = "banana "
+        self.ingr3 = "chicken slices "
+        self.sause = "sweet sauce "
+        self.additional = ""
+        self.addPrice = 10
+
+    def showIng(self):
+        return self.ingr1 + self.ingr2 + self.ingr3 + self.sause
+
+    def priceCheck(self):
+        if self.additional == "":
+            return str(self.price * self.q)
+        else:
+            return str((self.price + self.addPrice) * self.q)
+
+    def addIng(self, ingr):
+        self.additional += " " + ingr
+
+    def customerPizza(self):
+        if self.additional == "":
+            wholeP = self.showIng()
+        else:
+            wholeP = self.showIng() + self.additional
+        return wholeP
+
+    def __str__(self):
+        return '{self.base}, {self.size} cm, {self.price}'.format(self=self)
+
+
+def chooseOption(chooser):
+    print("how many pizzas you want?")
+    num = int(input())
+    thisPizza = chooser(num)
+    print("your pizza is today's pizza with: " + thisPizza.showIng() + " something to add? yes/no")
+    answer = input()
+    ind = True
+    if answer == "yes":
+        while ind:
+            print("enter ingridient to add: ")
+            ing = input()
+            thisPizza.addIng(ing)
+            print("is this all? yes - for yes, no - for no")
+            answer1 = input()
+            if answer1 == "yes":
+                ind = False
+    print("your pizza is with: " + thisPizza.customerPizza() + " and total price: " + thisPizza.priceCheck())
+
+
 if __name__ == '__main__':
-    cust1 = potd1(3)
-    print(cust1.showIng());
-    cust1.addIng(" corn")
-    print("total price is: " + str(cust1.priceCheck()))
-    print("your order is ready, pizza with: " + cust1.customerPizza())
-
-    cust2 = potd2(1)
-    print(cust2.showIng());
-    cust1.addIng(" smth")
-    print("total price is: " + str(cust2.priceCheck()))
-    print("your order is ready, pizza with: " + cust2.customerPizza())
-
-    cust3 = potd3(3)
-    print(cust3.showIng());
-    cust3.addIng(" smth")
-    print("total price is: " + str(cust3.priceCheck()))
-    print("your order is ready, pizza with: " + cust3.customerPizza())
-
-
-
+    weekday = datetime.datetime.today().weekday()
+    if weekday == 0:
+        chooseOption(MondayP)
+    if weekday == 1:
+        chooseOption(TuesdayP)
+    if weekday == 2:
+        chooseOption(WednesdayP)
+    if weekday == 3:
+        chooseOption(ThursdayP)
+    if weekday == 4:
+        chooseOption(FridayP)
+    if weekday == 5:
+        chooseOption(SaturdayP)
+    if weekday == 6:
+        chooseOption(SundayP)
