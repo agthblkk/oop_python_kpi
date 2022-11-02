a6f476a6911b69bca10efa87404c6c819987625d
agthblkk <101601410+agthblkk@users.noreply.github.com>
2022-11-02 20:53:05 +0200

    Update tas01LAB02_PART01.py

--- tas01LAB02_PART01.py
+++ tas01LAB02_PART01.py
@@ -1,7 +1,14 @@
+import json
+import datetime
+from random import randrange
+
+
 class Ticket:
-    def __init__(self, id):
+    def __init__(self, id, event, date):
         if isinstance(id, int):
             self.id = id
+            self.event = event
+            self.date = str(date)
             self.price = 10
             self.total = 0
         else:
@@ -17,12 +24,12 @@ class Ticket:
         return self.total
 
     def __str__(self):
-        return '{self.id}, {self.price}'.format(self=self)
+        return '{self.id}, {self.price} for {self.event}, {self.date}'.format(self=self)
 
 
 class Advanceticket(Ticket):
-    def __init__(self, id):
-        super().__init__(id)
+    def __init__(self, id, event, date):
+        super().__init__(id, event, date)
         self.priceA = self.price - (self.price * 40 / 100)
         self.total = 0
 
@@ -36,12 +43,12 @@ class Advanceticket(Ticket):
         return self.total
 
     def __str__(self):
-        return 'A ticket number {self.id}, costs {self.priceA}'.format(self=self)
+        return 'A ticket number {self.id}, costs {self.priceA} for {self.event}, {self.date}'.format(self=self)
 
 
 class Studentticket(Ticket):
-    def __init__(self, id):
-        super().__init__(id)
+    def __init__(self, id, event, date):
+        super().__init__(id, event, date)
         self.priceS = self.price - (self.price * 50 / 100)
         self.total = 0
 
@@ -55,12 +62,12 @@ class Studentticket(Ticket):
         return self.total
 
     def __str__(self):
-        return 'A ticket number {self.id}, costs {self.priceS}'.format(self=self)
+        return 'A ticket number {self.id}, costs {self.priceS} for {self.event}, {self.date}'.format(self=self)
 
 
 class Lateticket(Ticket):
-    def __init__(self, id):
-        super().__init__(id)
+    def __init__(self, id, event, date):
+        super().__init__(id, event, date)
         self.priceL = self.price + (self.price * 10 / 100)
         self.total = 0
 
@@ -74,17 +81,61 @@ class Lateticket(Ticket):
         return self.total
 
     def __str__(self):
-        return 'A ticket number {self.id}, costs {self.priceL}'.format(self=self)
+        return 'A ticket number {self.id}, costs {self.priceL} for {self.event}, {self.date}'.format(self=self)
+
+
+def write(data, filename):
+    data = json.dumps(data)
+    data = json.loads(str(data))
+    with open(filename, 'w', encoding='utf-8') as file:
+        json.dump(data, file, indent=4)
+
+
+def read(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        return json.load(file)
+
+
+def randomDate(start, end):
+    diff = end - start
+    intDelta = (diff.days * 24 * 60 * 60) + diff.seconds
+    random_second = randrange(intDelta)
+    return start + datetime.timedelta(seconds=random_second)
+
+
+# def diffBetweenTwoDates(d1, d2):
+#     delta = d2 - d1
+#     return delta.days
 
 
 if __name__ == '__main__':
-    t = Ticket(1)
-    tS = Studentticket(2)
-    tA = Advanceticket(1)
-    tL = Lateticket(3)
-    tS.buy(11)
-    tA.buy(3)
-    tL.buy(1)
-    t.buy(3)
-    print(str(t.totalP()) + " for regular tickets, " + str(tS.totalP()) + " for student tickets")
-    print(str(tL.totalP()) + " for late tickets, " + str(tA.totalP()) + " for advance tickets")
+    # random date between which we will choose next dates of events and current date
+    d1 = datetime.datetime.strptime('1/1/2019 1:30 PM', '%m/%d/%Y %I:%M %p')
+    d2 = datetime.datetime.strptime('6/1/2019 1:30 AM', '%m/%d/%Y %I:%M %p')
+    d3 = datetime.datetime.strptime('1/1/2020 1:30 AM', '%m/%d/%Y %I:%M %p')
+
+    dayOfBuyingATicket = randomDate(d1, d2)
+    data1 = randomDate(dayOfBuyingATicket, d3)
+    data2 = randomDate(dayOfBuyingATicket, d3)
+    data3 = randomDate(dayOfBuyingATicket, d3)
+    event1 = "StarWars Event"
+    event2 = "Java Event"
+    event3 = "Saturnalia Event"
+
+    # delta1 = diffBetweenTwoDates(dayOfBuyingATicket, data1)
+    # delta2 = diffBetweenTwoDates(dayOfBuyingATicket, data2)
+    # delta3 = diffBetweenTwoDates(dayOfBuyingATicket, data3)
+
+    dict = {}
+
+    ticket1 = Ticket(1, event1, data1)
+    ticket2 = Advanceticket(2, event2, data2)
+
+    dict["ticket1"] = ticket1.__dict__
+    dict["ticket2"] = ticket2.__dict__
+
+    write(dict, "data.json")
+
+    m = read("data.json")
+    for item in dict:
+        print(m[item]['id'])
